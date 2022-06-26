bde7d2ee9b8df8fc03b99ca518df0083bd9321a6
孙永刚 <1660998482@qq.com>
2022-06-26 21:09:31 +0800

    feat: search完成

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40 group"
+    ref="container"
   >
     <div>
       <a-svg-icon
@@ -12,8 +13,16 @@
         type="text"
         class="block w-full h-[44px] pl-4 outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide font-semibold border border-zinc-100 focus:border-red-300 text-sm duration-500 group-hover:bg-white group-hover:border-zinc-200"
         placeholder="搜索"
+        v-model="modelValue"
+        @keyup.enter="$emit('search', $event)"
+        @focus="onFocus"
+        @blur="onBlur"
+        @change="onChange"
+        @input="onInput"
       />
       <a-svg-icon
+        v-show="modelValue.length > 0"
+        @click="onClear"
         name="input-delete"
         class="h-1.5 w-1.5 absolute translate-y-[-50%] top-[50%] right-9 cursor-pointer duration-500"
       ></a-svg-icon>
@@ -23,12 +32,13 @@
       <a-button
         icon="search"
         icon-color="#fff"
+        @click="$emit('search', $event)"
         class="absolute translate-y-[-50%] top-[50%] right-1 rounded-full"
       ></a-button>
     </div>
-    <transition name="slide">
+    <transition name="slide" v-if="dropdown" v-show="isFocus">
       <div
-        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl"
+        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl opacity-0 group-hover:opacity-100"
       >
         <slot name="dropdown"></slot>
       </div>
@@ -36,7 +46,55 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
+import { PropType, ref } from 'vue'
+const container = ref<HTMLElement>()
+onClickOutside(container, (e: PointerEvent) => {
+  isFocus.value = false
+  emit('blur', e)
+})
+const emit = defineEmits([
+  'update:modelValue',
+  'search',
+  'focus',
+  'blur',
+  'clear',
+  'change',
+  'input'
+])
+
+defineProps({
+  modelValue: {
+    required: true,
+    type: String as PropType<string>
+  },
+  dropdown: {
+    type: Boolean as PropType<boolean>,
+    default: false
+  }
+})
+function onFocus(e: FocusEvent) {
+  isFocus.value = true
+  emit('focus', e)
+}
+function onBlur(e: FocusEvent) {
+  emit('blur', e)
+}
+function onClear() {
+  emit('update:modelValue', '')
+  emit('clear', '')
+}
+function onChange(e: Event) {
+  const input = e.target as HTMLInputElement
+  emit('change', input.value)
+}
+function onInput(e: Event) {
+  const input = e.target as HTMLInputElement
+  emit('input', input.value)
+}
+const isFocus = ref(false)
+</script>
 
 <style lang="scss" scoped>
 .slide-enter-active,
--- src/views/layout/components/header/search/index.vue
+++ src/views/layout/components/header/search/index.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <a-search></a-search>
+    <a-search v-model="inputValue" @search="onSearch" :dropdown="true">
+      <template #dropdown> this is template </template>
+    </a-search>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const inputValue = ref('')
+
+function onSearch() {
+  console.log(inputValue)
+}
+</script>
 
 <style></style>
